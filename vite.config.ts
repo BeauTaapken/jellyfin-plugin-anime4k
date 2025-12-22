09e3cb42ba029b1564db4e8cb534cda1171537da
chip <86570889+chip44@users.noreply.github.com>
2025-12-22 15:12:59 +0200

    remove console.log calls in production builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-  build: {
-    lib: {
-      entry: 'src/main.ts',
-      formats: ['es', 'iife'],
-      name: 'Anime4KPlugin',
+export default defineConfig(({ mode }) => {
+  return {
+    build: {
+      lib: {
+        entry: 'src/main.ts',
+        formats: ['es', 'iife'],
+        name: 'Anime4KPlugin',
+      },
     },
-  },
+    esbuild: {
+      pure: mode === 'production' ? ['console.log'] : [],
+    },
+  };
 });
